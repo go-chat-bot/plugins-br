95ef6f76738fe673ae0fd542dea01791b81a22a8
Breno Martinusso <martinusso@gmail.com>
2016-10-22 16:28:04 -0200

    Muda API (#7)

--- cotacao/cotacao.go
+++ cotacao/cotacao.go
@@ -2,28 +2,24 @@ package cotacao
 
 import (
 	"fmt"
+
 	"github.com/go-chat-bot/bot"
 	"github.com/go-chat-bot/plugins/web"
 )
 
 var (
-	url = "http://developers.agenciaideias.com.br/cotacoes/json"
+	url = "https://economia.awesomeapi.com.br/json/all"
 )
 
 type retorno struct {
-	Bovespa struct {
-		Cotacao  string `json:"cotacao"`
-		Variacao string `json:"variacao"`
-	} `json:"bovespa"`
 	Dolar struct {
-		Cotacao  string `json:"cotacao"`
-		Variacao string `json:"variacao"`
-	} `json:"dolar"`
+		Cotacao  string `json:"ask"`
+		Variacao string `json:"varBid"`
+	} `json:"USD"`
 	Euro struct {
-		Cotacao  string `json:"cotacao"`
-		Variacao string `json:"variacao"`
-	} `json:"euro"`
-	Atualizacao string `json:"atualizacao"`
+		Cotacao  string `json:"ask"`
+		Variacao string `json:"varBid"`
+	} `json:"EUR"`
 }
 
 func cotacao(command *bot.Cmd) (msg string, err error) {
